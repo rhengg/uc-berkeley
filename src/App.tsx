467dfef4d4e9514092508e39e08c8c238b2e91ab
Pranab Bora <prnbbora4@gmail.com>
2023-10-30 17:38:44 +0530

    typography, button and created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card';
+import './css/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '2rem', alignItems: 'center' }}>
+      <button className='large-primary-btn'>Button Primary</button>
+      <button className='large-secondary-btn'>Button Primary</button>
+      <button className='small-primary-btn'>Button Primary</button>
+      <button className='small-secondary-btn'>Button Primary</button>
+
+      <Card
+        name='Arijit Singh'
+        origin='India'
+      />
     </div>
   );
 }
