588f8cab56df5a73859a3729caa50da4d6f5830f
Chinmoy Raj Deka <rajchinmoy007@gmail.com>
2023-10-31 17:19:36 +0530

    css changed

--- src/layout/home/index.tsx
+++ src/layout/home/index.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navigation from '../../components/Navigation'
 
 const Index = () => {
 
     return (
-        <>
-            <div className="nav-container">
-                <div className="nav-left">
-                    <div className='auth-layout-content-circle'></div>
-                    <p className="subtitle-two">South Asian Musicians</p>
-                </div>
-                <div className="nav-right">
-                    <p className="subtitle-two">John Doe</p>
-                    <button className="large-secondary-btn"> Sign Out </button>
-                </div>
-            </div>
+        <div style={{ width: "100%" }}>
+            <Navigation />
             <div className='home-layout-body'>
                 <Outlet />
             </div>
-        </>
+        </div>
     )
 }
 
