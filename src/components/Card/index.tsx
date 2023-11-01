5e6b5867ce44e980a7f3f576183f25a71a081b33
Pranab Bora <prnbbora4@gmail.com>
2023-11-01 12:42:27 +0530

    card modified

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdNorthEast, MdBookmarkBorder } from 'react-icons/md'
+import { MdNorthEast } from 'react-icons/md'
 
 type CardProps = {
 	name: string
@@ -7,12 +7,13 @@ type CardProps = {
 	image?: string
 	origin?: string
 	handleCardClick: (id: string) => void
+	genre?: string
 }
 
 
 
 const Card = (props: CardProps) => {
-	const { id, name, image, origin, handleCardClick } = props
+	const { id, name, image, origin, handleCardClick, genre } = props
 
 	return (
 		<div className="card-container">
@@ -28,33 +29,27 @@ const Card = (props: CardProps) => {
 			<div className="details-holder">
 				<p className="subtitle-two">{name}</p>
 
-				<div className="tag-container">
+				<div style={{ marginTop: '1rem' }}>
 					<div className="tag">
 						<p className="body">{origin}</p>
 					</div>
 				</div>
 
-				<div className="tag-container">
-					<div className="tag">
-						<p className="body">Pop</p>
-					</div>
-					<div className="tag">
-						<p className="body">Asian</p>
-					</div>
-					<div className="tag">
-						<p className="body">Others</p>
-					</div>
+				<div className="tag-container hide-scroll">
+					{
+						genre?.split(",").map((genre: string, index: number) => {
+							return (
+								<div className="tag" key={index}>
+									<p className="body">{genre}</p>
+								</div>
+							)
+						}
+						)
+					}
 
 				</div>
-
-
-
 			</div>
-			<div className="card-icon-holder">
-				<div className="icon">
-					<MdBookmarkBorder />
-				</div>
-			</div>
+
 			<div className="card-icon-holder">
 				<div className="icon" onClick={() => handleCardClick(id as string)}>
 					<MdNorthEast />
