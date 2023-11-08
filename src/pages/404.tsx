fffb50203b8ae31b90b0c24a1251be2a63292de1
Pranab Bora <prnbbora4@gmail.com>
2023-11-08 16:37:32 +0530

    comments in page and components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Error from '../components/Error'
 
+/**
+ * This page will render if the user enter the wrong path name.
+ */
+
 const NotFound = () => {
     return (
         <Error errorMessage='Page Not Found' />
